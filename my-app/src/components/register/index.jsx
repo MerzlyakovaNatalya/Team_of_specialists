b9f3c605e24488c38eab0e1b1b9431120b0eb6b6
MerzlyakovaNatalya <94801202+MerzlyakovaNatalya@users.noreply.github.com>
2022-12-22 12:43:51 +0300

    update_register

--- my-app/src/components/register/index.jsx
+++ my-app/src/components/register/index.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
 import style from './Register.module.scss'
+import look from '../../image/look.png'
 
 export const Register = () => {
     return (
-        <div>
-            <h1>Register</h1>
+        <div className={style.wrap}>
+            <div className={style.content}>
+                <h2 className={style.title}>Регистрация</h2>
+                <form className={style.form}>
+                <p className={style.text}>Имя</p>
+                <input type="text" className={style.input}/>
+                <p className={style.text}>Электронная почта</p>
+                <input type="text" className={style.input}/>
+                <p className={style.text}>Пароль</p>
+                <div className={style.wrap_input_password}>
+                   <input 
+                     type="text" 
+                     className={style.input}
+                     placeholder="******"/>
+                   <button className={style.button_password}>
+                     <img src={look} alt=''/>
+                   </button>
+                </div>
+                <p className={style.text}>Подтвердите пароль</p>
+                <div className={style.wrap_input_password}>
+                   <input 
+                     type="text" 
+                     className={style.input}
+                     placeholder="******"/>
+                   <button className={style.button_password}>
+                     <img src={look} alt=''/>
+                   </button>
+                </div>
+                <button className={style.button}>Зарегистрироваться</button>
+                </form>
+            </div>
         </div>
     )
 }
--- my-app/src/components/usersList/index.jsx
+++ my-app/src/components/usersList/index.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
 import style from './UserList.module.scss'
-import axios from 'axios'
 import { Loader } from '../loader'
 import { Error } from '../error'
 import { useDispatch, useSelector } from 'react-redux'
 import { getUsers } from '../../store/users/actions'
 import { getUsersData, getError, getLoading } from '../../store/users/selectors'
 import { UserCard } from '../userСard'
+import { Modal } from '../modal'
+import { Register } from '../register'
 
 export const UserList = () => {
 
   const users = useSelector(getUsersData);
   const error = useSelector(getError);
   const loading = useSelector(getLoading);
+  const [isAuth, setIsAuth] = useState(true);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -21,13 +23,21 @@ export const UserList = () => {
   }, [])
 
   return (
-    <div>
+    <div className={style.wrap}>
+      <div className={style.header}>
+       <button className={style.button}>Выход</button>
+       <h1 className={style.title}>Наша команда</h1>
+       <p className={style.text}>Это опытные специалисты, хорошо разбирающиеся во всех задачах, которые ложатся на их плечи, и умеющие находить выход из любых, даже самых сложных ситуаций. </p>
+      </div>
       {error && <Error />}
       {loading && <Loader />}
       <h1>UserList</h1>
       {users.map((item) => (
         <UserCard key={item.id} {...item}/>
       ))}
+      {isAuth && <Modal>
+          <Register/>
+        </Modal>}
     </div>
   )
 }
