e5076efa6f96182ced765566efd42b262c6e9b6b
MerzlyakovaNatalya <94801202+MerzlyakovaNatalya@users.noreply.github.com>
2022-12-27 00:18:58 +0300

    commit_add

--- my-app/src/hooks/useUserCard/index.js
+++ my-app/src/hooks/useUserCard/index.js
@@ -1,28 +1,38 @@
-import { useNavigate } from 'react-router-dom'
-import { getUsersData } from '../../store/users/selectors'
-import { useDispatch, useSelector } from 'react-redux'
-import { setIsAuth, reset } from '../../store/users/actions'
+import { useNavigate } from "react-router-dom";
+import { getUsersData } from "../../store/users/selectors";
+import { useDispatch, useSelector } from "react-redux";
+import { setIsAuth, reset } from "../../store/users/actions";
+import { useEffect, useState } from "react";
 
 export const useUserCard = (userId) => {
-    const users = useSelector(getUsersData)
-    const navigate = useNavigate()
-    const dispatch = useDispatch()
-  
-    const user = users.find(({ id }) => id === Number(userId))
-  
-    const harkBack = () => {
-      navigate(-1)
+  const [userFromStorage, setUserFromStorage] = useState("");
+  const users = useSelector(getUsersData);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const user = users.find(({ id }) => id === Number(userId));
+
+  const harkBack = () => {
+    dispatch(setIsAuth(false));
+    navigate(-1);
+  };
+
+  const getExit = () => {
+    navigate("/");
+    localStorage.removeItem("token_auth");
+    dispatch(setIsAuth(true));
+    dispatch(reset());
+  };
+
+  useEffect(() => {
+    if (user !== undefined) {
+      localStorage.setItem("user", JSON.stringify(user));
+      setUserFromStorage(user);
+    } else {
+      const item = JSON.parse(localStorage.getItem("user"));
+      setUserFromStorage(item);
     }
-  
-    const getExit = () => {
-      navigate('/')
-      localStorage.removeItem('token_auth')
-      dispatch(setIsAuth(true))
-      dispatch(reset())
-    }
-    return [
-        user,
-        harkBack,
-        getExit
-    ]
-}
+  }, []);
+
+  return [userFromStorage, harkBack, getExit];
+};
